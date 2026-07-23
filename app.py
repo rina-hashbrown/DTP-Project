c294576f9c63d02b83a00da7f6caf635e32878b6
rina-hashbrown <rinaaa.school@gmail.com>
2026-07-23 22:58:23 +1200

    trying to make SQLAlchemy work

--- app.py
+++ app.py
@@ -7,46 +7,38 @@ DATABASE = 'database.db'
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, ForeignKey, select
+from typing import Optional
+from sqlalchemy import Text
+import os
 
 # create the app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
 
-def get_db():
-    db = getattr(g, '_database.db', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+class Base(DeclarativeBase):
+    pass
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+class Rocket(Base):
+    __tablename__ = "rocket"
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+    # Primary Key
+    rocket_id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Rocket Name (TEXT -> str)
+    rocket_name: Mapped[str] = mapped_column(String(100))
+
+    # Rocket Status (TEXT -> str)
+    rocket_status: Mapped[str] = mapped_column(String(50))
+
+    # Image URL (TEXT DEFAULT ->  str)
+    image_url: Mapped[Optional[str]] = mapped_column("imageURL", Text(), nullable=True)
 
 @app.route('/')
-def home(): 
-    #rocketpage - -- simple query--
-    sql = """
-                SELECT rocket.rocket_ID, rocket.rocket_name, rocket.rocket_status, rocket.imageURl
-                FROM rocket;"""
-    results = query_db(sql)
-    return render_template("home.html", results=results)
-
-@app.route("/rocket/<int:id>")
-def rocket(id):
-    #just a singular rocket based on id
-    sql = """SELECT * FROM rocket JOIN rocket_location ON rocket_location.rocket_ID = rocket.rocket_ID
-            WHERE rocket.rocket_ID =?;"""
-    result = query_db(sql,(id,),True)
-    return str(result)
+def get_rockets():  # Changed function name to avoid conflicts
+    rockets = db.session.execute(select(Rocket)).scalars().all()  # Plural variable
+    return render_template('rockets.html', rockets=rockets)
 
 if __name__ == "__main__":
     app.run(debug=True) 

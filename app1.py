13b77e19425bceac0610fb1bcf77291f99f96a75
rina-hashbrown <rinaaa.school@gmail.com>
2026-07-23 12:28:31 +1200

    focus on 1 python file

--- app1.py
+++ app1.py
@@ -1,7 +1,20 @@
-from flask import Flask, g, render_template
+from flask import Flask, render_template
 import sqlite3
 
 DATABASE = 'database.db'
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Looks inside the 'templates' folder for 'home.html'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 #initialise app
 app = Flask(__name__)
